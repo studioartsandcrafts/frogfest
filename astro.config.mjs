80d1175d5e21bc6324390a8490b4dcdf0f764aec
last night <fujizhang2525@gmail.com>
2023-08-10 05:50:41 -0400

    add tailwind and svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://studioartsandcrafts.github.io',
+  site: 'https://studioartsandcrafts.github.io',
+  integrations: [tailwind(), svelte()]
 });
