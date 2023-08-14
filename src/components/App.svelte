d37abd4a3491cf981b5d0c90dead0057ca5f8749
last night <fujizhang2525@gmail.com>
2023-08-13 23:02:19 -0400

    header

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,7 +1,12 @@
+<script lang="ts">
+  import Header from "./Header.svelte";
+</script>
+
+<Header />
 <div>9anime</div>
 
 <style lang="postcss">
   div {
-    height: 90rem;
+    height: 900rem;
   }
 </style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,46 @@
+<script>
+  import TopIcons from "./TopIcons.svelte";
+</script>
+
+<header>
+  <div>
+    <p>8 — 11PM PST <span class="ml-4">AUGUST 18 </span></p>
+    <p>
+      <span>W28</span>
+      <span class="ml-4">P35</span>
+      <span class="ml-4">MIST,</span>
+    </p>
+    <p>Primal Lamia</p>
+  </div>
+
+  <div class="list flex flex-col items-center">
+    <p>
+      <span>zwackery</span>
+      <span> cat wap b2b deej nuts </span>
+      <span> ronsrogue</span>
+    </p>
+    <p>
+      <span> ocoenia b2b vapor cruise</span>
+      <span> babubii b2b super d</span>
+      <span>sleef</span>
+    </p>
+    <p>
+      <span>mega flare</span>
+      <span> good doggo licks </span>
+    </p>
+  </div>
+  <div class="flex flex-col items-end">
+    <p><a href="https://frogfest">www.frogfest.live</a></p>
+    <TopIcons />
+  </div>
+</header>
+
+<style lang="postcss">
+  header {
+    @apply w-full sticky flex justify-between top-0 uppercase font-bold z-50;
+    @apply px-2 py-1 leading-tight;
+  }
+  .list p {
+    @apply inline-flex gap-4;
+  }
+</style>
