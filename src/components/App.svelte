4cd5ed61f70381f78e34eadba2d8ca048df72b33
last night <fujizhang2525@gmail.com>
2023-08-15 02:53:43 -0400

    test

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -8,6 +8,7 @@
   class="mt-2 mx-2 flex flex-col items-center justify-center text-white px-5 lg:px-0 text-center rounded-[7rem]"
 >
   <img data-depth="5" src="girl.png" alt="frogfest magical girl" />
+  test
 </section>
 
 <style lang="postcss">
